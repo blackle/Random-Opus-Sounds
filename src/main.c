e75c07e9a678f8502ee0a85434108379a425d635
blackle <isabelle@blackle-mori.com>
2025-01-08 15:57:17 -0500

    Code cleanup

--- src/main.c
+++ src/main.c
@@ -18,9 +18,49 @@ void random_packet(uint32_t seed) {
 		state = state ^ (state << 13u);
 		state = state ^ (state >> 17u);
 		state = state ^ (state << 5u);
-		// state *= 1685821657u;
+		state *= 1685821657u;
 		PACKET[i] = state;
 	}
+}
+
+void play_random_packet(uint32_t seed, ao_device* device) {
+	random_packet(seed);
+	OpusDecoder* opus_decoder = opus_decoder_create(SAMPLE_RATE, 1, NULL);
+
+	float decoded_data[DECODED_DATA_SIZE];
+	int length;
+	length = opus_decode_float(opus_decoder, PACKET, PACKET_SIZE, decoded_data, DECODED_DATA_SIZE, 0);
+	(void)length;
+
+	float last_rms = 0.;
+	for (int i = 0; i < 10; i++) {
+		float rms = 0;
+		for (int i = 0; i < DECODED_DATA_SIZE; i++) {
+			rms += pow(decoded_data[i], 2);
+		}
+		rms = sqrt(rms/DECODED_DATA_SIZE);
+		if (i == 0) {
+			last_rms = rms;
+		}
+		int16_t converted_data[DECODED_DATA_SIZE];
+		for (int i = 0; i < DECODED_DATA_SIZE; i++) {
+			// fade in to the next rms
+			float t = ((float)i)/(DECODED_DATA_SIZE-1);
+			float thisscale = t*fmax(last_rms,rms) + (1-t)*last_rms;
+			converted_data[i] = tanh(decoded_data[i]/thisscale/4.) * INT16_MAX;
+		}
+		last_rms = fmax(rms,last_rms);
+		ao_play(device, (char*)converted_data, DECODED_DATA_SIZE*sizeof(int16_t));
+		if (i < 0) {
+			random_packet(seed);
+			length = opus_decode_float(opus_decoder, PACKET, PACKET_SIZE, decoded_data, DECODED_DATA_SIZE, 0);
+		} else {
+			// discontinuous transmission
+			length = opus_decode_float(opus_decoder, PACKET, PACKET_SIZE, decoded_data, DECODED_DATA_SIZE, 1);
+		}
+		(void)length;
+	}
+	opus_decoder_destroy(opus_decoder);
 }
 
 int main(int argc, char** argv) {
@@ -38,58 +78,17 @@ int main(int argc, char** argv) {
 		.value = "200",
 		.next = NULL,
 	};
+	(void) option;
 	ao_device* device = ao_open_live(ao_driver_id("pulse"), &format, &option);
-	// ao_device* device = ao_open_file(ao_driver_id("wav"), "samples.wav", 1, &format, &option);
-	(void) device;
 
-	for (int j = 0; j < 32; j++) {
-	random_packet(42342+j);
-	OpusDecoder* opus_decoder = opus_decoder_create(SAMPLE_RATE, 1, NULL);
-	for (int k = 0; k < 4; k++) {
-	float decoded_data[DECODED_DATA_SIZE];
-	int length;
-	length = opus_decode_float(opus_decoder, PACKET, PACKET_SIZE, decoded_data, DECODED_DATA_SIZE, 0);
-	(void)length;
-	float rms = 0.;
-
-	// float top = decoded_data[DECODED_DATA_SIZE-1];
-	// float bottom = decoded_data[0];
-	// float avg = (top+bottom)/2;
-
-	for (int i = 0; i < DECODED_DATA_SIZE; i++) {
-		// float t = ((float)i)/(DECODED_DATA_SIZE-1);
-		// decoded_data[i] -= t*top + (1-t)*bottom - avg;
-		rms += pow(decoded_data[i], 2);
+	for (int j = 0; j < 100; j++) {
+		// todo: not working for some reason :c
+		// char filename[80];
+		// snprintf(filename, 80, "samples/opus_%02d.wav", j);
+		// printf("writing %s\n", filename);
+		// ao_device* device = ao_open_file(ao_driver_id("wav"), filename, 1, &format, NULL);
+		play_random_packet(j, device);
 	}
-	rms = sqrt(rms/DECODED_DATA_SIZE);
-
-	for (int i = 0; i < 10; i++) {
-		float rms2 = 0;
-		for (int i = 0; i < DECODED_DATA_SIZE; i++) {
-			// float t = ((float)i)/(DECODED_DATA_SIZE-1);
-			// decoded_data[i] -= t*top + (1-t)*bottom - avg;
-			rms2 += pow(decoded_data[i], 2);
-		}
-		rms2 = sqrt(rms2/DECODED_DATA_SIZE);
-		int16_t converted_data[DECODED_DATA_SIZE];
-		for (int i = 0; i < DECODED_DATA_SIZE; i++) {
-			float t = ((float)i)/(DECODED_DATA_SIZE-1);
-			float thisscale = t*fmax(rms,rms2) + (1-t)*rms;
-			converted_data[i] = tanh(decoded_data[i]/thisscale/4.) * INT16_MAX;
-		}
-		rms = fmax(rms,rms2);
-		ao_play(device, (char*)converted_data, DECODED_DATA_SIZE*sizeof(int16_t));
-		if (i < 0) {
-			random_packet(422+j%4*7483+i*432);
-			length = opus_decode_float(opus_decoder, PACKET, PACKET_SIZE, decoded_data, DECODED_DATA_SIZE, 0);
-		} else {
-			length = opus_decode_float(opus_decoder, PACKET, PACKET_SIZE, decoded_data, DECODED_DATA_SIZE, 1);
-		}
-
-		(void) length;
-	}
-	}
-}
 
 	return 0;
 }
